736e8e05b121340a1876603081a8b98017812ad3
ze <jr.carvalho@usp.br>
2023-08-31 03:44:28 +0000

    Pastas python 1 add

---
+++ name.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+
+prPurple('jose_renato')
---
+++ teste.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import sys
+algo = sys.argv[1]
+ogla = sys.argv[2]
+print(algo, 'e', ogla, 'foram computados')
