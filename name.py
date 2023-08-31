877febf7811becbd67f7389b505458fc7e0948dd
ze <jr.carvalho@usp.br>
2023-08-31 04:13:39 +0000

    Remover lixo e adicionar python 1

--- name.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
-
-prPurple('jose_renato')
--- teste.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-algo = sys.argv[1]
-ogla = sys.argv[2]
-print(algo, 'e', ogla, 'foram computados')
