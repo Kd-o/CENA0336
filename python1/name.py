c652b1fad7b0da0bf2a1802a8bdde68686b32268
ze <jr.carvalho@usp.br>
2023-08-31 04:11:31 +0000

    Excericio

--- python1/name.py
+++ python1/name.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
-eu = 'jose renato /'
-print(eu,'Roxo /','Fazer bolos /','Gatos')
+import sys
+nome = sys.argv[1]
+cor = sys.argv[2]
+atividade =  sys.argv[3]
+animal = sys.argv[4]
+print(nome,',',cor,',',atividade,',',animal)
+print(nome,'+',cor,'+',atividade,'+',animal)
+#'jose renato' 'roxo' 'fazer bolos' 'gatos'
